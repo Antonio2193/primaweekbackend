946fa1ac7379deb6174e7ff00d966cc5dc9225e2
Antonio2193 <161360723+Antonio2193@users.noreply.github.com>
2024-09-09 10:10:01 +0200

    centrati pulsanti per cambiare pagina

--- frontEnd/src/components/blog/blog-list/BlogList.jsx
+++ frontEnd/src/components/blog/blog-list/BlogList.jsx
@@ -3,6 +3,7 @@ import { Col, Row, Form, Button } from "react-bootstrap";
 import BlogItem from "../blog-item/BlogItem";
 import { loadPosts } from "../../../data/fetch";
 import { AuthorContext } from "../../../context/AuthorContextProvider";
+import style from "./style.css";
 
 const BlogList = () => {
   const { token } = useContext(AuthorContext);
